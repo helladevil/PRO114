1e1ca4aeaa06e0e51b1f46820d32b7e7bb8c0316
helladevil <127803793+helladevil@users.noreply.github.com>
2024-05-31 17:48:11 -0300

    Add files via upload

---
+++ PRO114/test1.py
@@ -0,0 +1,8 @@
+a = 33
+b = "33"
+if b > a:
+    print("b es mayor que a")
+elif a == b:
+    print("a y b son iguales")
+#la cantidad aca es la misma por en de no se podrá mostrar algo asi de como que son distintos
+ 
